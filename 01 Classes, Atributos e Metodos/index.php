848618f835ddc8247557a001c00f4f31eaceda42
Miguel Henrique <miguelhp373@gmail.com>
2021-02-15 18:51:57 -0300

    Aula 01 - concluida

---
+++ 01 Classes, Atributos e Metodos/index.php
@@ -0,0 +1,21 @@
+<?php
+
+//objeto
+class Pessoas{
+    //atributos
+    public $nome;
+    public $idade;
+
+    //metodo
+    public function Falar(){
+        echo 'Olá Eu Me Chamo '.$this->nome.' e Tenho '.$this->idade.' anos';
+    }
+}
+
+$Jonas = new Pessoas();
+$Jonas->nome = 'Jonas';
+$Jonas->idade = 17;
+//var_dump($Jonas);
+$Jonas->Falar();
+
+?>
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # PHP_Orientado_a_Objetos
- Curso de POO do Canal Node Studio Treinamentos
